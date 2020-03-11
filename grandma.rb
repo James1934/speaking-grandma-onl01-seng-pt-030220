13ab977ad455f5a5a58a3156cb1b2d9397fa615d
Bartholomew Fountain <baertholomew@gmail.com>
2020-03-11 20:35:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  else
-    return anwser
-  end
+  return "NO, NOT SINCE 1938!"
+else
+  return answer
+end
 end
